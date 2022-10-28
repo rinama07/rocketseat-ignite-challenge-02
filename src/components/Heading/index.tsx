894674a3945608600e1976e55eb74ebc01915e3d
Marina <marinampq7@gmail.com>
2022-10-28 20:11:41 -0300

    create heading components

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -1,11 +1,27 @@
-import { HTMLAttributes } from 'react';
+import { ReactNode } from 'react';
 
-import { HeadingText, HeadingVariant } from './styles';
+import { ThemeColor } from '../../styles/themes/default';
 
-interface HeadingProps extends HTMLAttributes<HTMLHeadingElement> {
-  variant: HeadingVariant;
+import { Heading1, Heading2, Heading3, Heading4 } from './styles';
+
+interface HeadingProps {
+  element?: 'h1' | 'h2' | 'h3' | 'h4';
+  color?: ThemeColor;
+  children?: ReactNode;
 }
 
-export function Heading(props: HeadingProps) {
-  return <HeadingText {...props} />;
+export function Heading({
+  element = 'h1',
+  color = 'brown700',
+  children,
+}: HeadingProps) {
+  if (element === 'h1') {
+    return <Heading1 color={color}>{children}</Heading1>;
+  } else if (element === 'h2') {
+    return <Heading2 color={color}>{children}</Heading2>;
+  } else if (element === 'h3') {
+    return <Heading3 color={color}>{children}</Heading3>;
+  } else {
+    return <Heading4 color={color}>{children}</Heading4>;
+  }
 }
--- src/components/Heading/styles.ts
+++ src/components/Heading/styles.ts
@@ -1,14 +1,39 @@
 import styled from 'styled-components';
 
-import { defaultTheme } from '../../styles/themes/default';
-
-export type HeadingVariant = keyof typeof defaultTheme.color;
+import { ThemeColor } from '../../styles/themes/default';
 
 interface HeadingProps {
-  variant: HeadingVariant;
+  color: ThemeColor;
 }
 
-export const HeadingText = styled.h1<HeadingProps>`
-  color: ${(props) => props.theme.color[props.variant]};
-  margin-bottom: 1rem;
+export const Heading1 = styled.h1<HeadingProps>`
+  color: ${(props) => props.theme.color[props.color]};
+  font-family: 'Baloo 2', cursive;
+  font-size: ${(props) => props.theme.font.heading.xl};
+  font-weight: 800;
+  line-height: 1.3;
+`;
+
+export const Heading2 = styled.h2<HeadingProps>`
+  color: ${(props) => props.theme.color[props.color]};
+  font-family: 'Baloo 2', cursive;
+  font-size: ${(props) => props.theme.font.heading.lg};
+  font-weight: 800;
+  line-height: 1.3;
+`;
+
+export const Heading3 = styled.h3<HeadingProps>`
+  color: ${(props) => props.theme.color[props.color]};
+  font-family: 'Baloo 2', cursive;
+  font-size: ${(props) => props.theme.font.heading.md};
+  font-weight: 700;
+  line-height: 1.3;
+`;
+
+export const Heading4 = styled.h4<HeadingProps>`
+  color: ${(props) => props.theme.color[props.color]};
+  font-family: 'Baloo 2', cursive;
+  font-size: ${(props) => props.theme.font.heading.sm};
+  font-weight: 700;
+  line-height: 1.3;
 `;
