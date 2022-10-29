9b54f2815858a3452f22bb6177fa3c87ac49b5d2
Marina <marinampq7@gmail.com>
2022-10-29 08:39:23 -0300

    fetch products for home page

--- src/pages/HomePage/ProductSection/index.tsx
+++ src/pages/HomePage/ProductSection/index.tsx
@@ -1,30 +1,17 @@
 import { useEffect, useState } from 'react';
-
 import { Heading } from '../../../components/Heading';
-import { ProductData } from '../../../types/api/product';
+import { getProducts } from '../../../server/getProducts';
 import { MenuProduct } from '../../../types/product';
-import { formatNumberToCurrency } from '../../../utils/number';
 
 import { messages } from './messages';
 import { ProductCard } from './ProductCard';
 import { ProductList, SectionContainer } from './styles';
 
 export function ProductSection() {
-  const [productList, setProductList] = useState<MenuProduct[]>([]);
+  const [products, setProducts] = useState<MenuProduct[]>([]);
 
   useEffect(() => {
-    fetch('http://localhost:3004/products')
-      .then((response) => response.json())
-      .then((data: ProductData[]) => {
-        setProductList(
-          data.map((product) => ({
-            ...product,
-            categories: product.categories?.split(',') ?? [],
-            imageSrc: 'src/assets/' + product.image?.src ?? '',
-            price: formatNumberToCurrency(product.unitPrice),
-          }))
-        );
-      });
+    getProducts().then((products) => setProducts(products));
   }, []);
 
   return (
@@ -34,7 +21,7 @@ export function ProductSection() {
       </Heading>
 
       <ProductList>
-        {productList.map((product) => (
+        {products.map((product: MenuProduct) => (
           <ProductCard key={product.id} product={product} />
         ))}
       </ProductList>
---
+++ src/server/getProducts.ts
@@ -0,0 +1,16 @@
+import { ProductData } from '../types/api/product';
+import { MenuProduct } from '../types/product';
+import { formatNumberToCurrency } from '../utils/number';
+
+export async function getProducts(): Promise<MenuProduct[]> {
+  const products: ProductData[] = await fetch('http://localhost:3004/products')
+    .then((response) => response.json())
+    .then((data: ProductData[]) => data);
+
+  return products.map((product) => ({
+    ...product,
+    categories: product.categories?.split(',') ?? [],
+    imageSrc: 'src/assets/' + product.image?.src ?? '',
+    price: formatNumberToCurrency(product.unitPrice),
+  }));
+}
